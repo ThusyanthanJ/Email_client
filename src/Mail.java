daace80bca05c253d7a85faf7a1673ee42fdce36
ThusyanthanJ <thushy1999@gmail.com>
2023-10-05 06:41:07 +0530

    Dependencies added

--- src/Mail.java
+++ src/Mail.java
@@ -1,8 +1,16 @@
-import java.security.MessageDigest;
-import java.util.Properties;
+import java.util.HashMap;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.*;
+import java.util.Properties;
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.Objects;
+import java.io.File;
 class Mail implements java.io.Serializable {
     static String mailAdd;
     static String mailpassword;
@@ -22,7 +30,7 @@ class Mail implements java.io.Serializable {
         properties.put("mail.smtp.host","smtp.gmail.com");
         properties.put("mail.smtp.port","587");
         String MyEmail = mailAdd;
-        String password = mailpassword;
+        String password = "zgna katg sdse eyex";
         Session session = Session.getInstance(properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -32,8 +40,7 @@ class Mail implements java.io.Serializable {
         Message message = sendingMail(session,MyEmail,Recepient,Subject,Content);
         Transport.send(message);
     }
-    private static Message sendingMail(Session session, String MyEmail,String Recepient, String
-            Subject,String Txt) throws MessagingException {
+    private static Message sendingMail(Session session, String MyEmail,String Recepient, String Subject,String Txt) throws MessagingException {
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(MyEmail));
         message.setRecipient(Message.RecipientType.TO,new InternetAddress(Recepient));
